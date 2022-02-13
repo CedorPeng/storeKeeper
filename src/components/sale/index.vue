6da441314a8b82764b2716012e8e64ade1df27a9
cedorpeng <cedorpeng@163.com>
2022-02-13 19:32:12 +0800

    '销售记录页面除接口外完成'

--- src/components/sale/index.vue
+++ src/components/sale/index.vue
@@ -9,7 +9,7 @@
           check-all
           clearable
           :options="productName.options"
-          v-model="productName.value"
+          v-model="searchParams.productId"
         ></dropTree>
       </div>
       <div class="searchItem">
@@ -20,13 +20,13 @@
           check-all
           clearable
           :options="channel.options"
-          v-model="channel.value"
+          v-model="searchParams.channelId"
         ></dropTree>
       </div>
       <div class="searchItem">
         <div class="searchLabel">购买客户：</div>
         <div class="currentInput">
-          <el-input v-model="buyer" placeholder="请输入客户名称" size="mini" clearable></el-input>
+          <el-input v-model="searchParams.buyers" placeholder="请输入客户名称" size="mini" clearable></el-input>
         </div>
       </div>
       <div class="searchItem">
@@ -37,19 +37,19 @@
           check-all
           clearable
           :options="memberName.options"
-          v-model="memberName.value"
+          v-model="searchParams.seller"
         ></dropTree>
       </div>
       <div class="searchItem">
         <div class="searchLabel">销售时间：</div>
         <div class="currentInput">
-<!--          <el-input v-model="salesTime" placeholder="请输入客户名称" size="mini" clearable></el-input>-->
           <el-date-picker
             size="mini"
-            v-model="salesTime"
+            v-model="searchParams.time"
             type="daterange"
             align="right"
             unlink-panels
+            value-format="yyyy-MM-dd"
             range-separator="-"
             start-placeholder="开始日期"
             end-placeholder="结束日期">
@@ -58,8 +58,8 @@
       </div>
       <div class="searchBtn">
         <el-button size="mini" type="primary" @click="addSales">新增销售记录</el-button>
-        <el-button size="mini" type="primary" plain>重 置</el-button>
-        <el-button size="mini" type="primary">查 询</el-button>
+        <el-button size="mini" type="primary" plain @click="resetSearch">重 置</el-button>
+        <el-button size="mini" type="primary" @click="clickSearch">查 询</el-button>
       </div>
     </div>
     <div class="salesMain">
@@ -70,42 +70,49 @@
         height="calc(100vh - 284px)">
         <el-table-column
           prop="time"
-          width="160"
+          width="200"
           :resizable="false"
           align="center"
           label="销售时间">
         </el-table-column>
         <el-table-column
-          prop="buyer"
+          prop="buyers"
           width="160"
           :resizable="false"
           align="center"
           label="购买客户">
         </el-table-column>
         <el-table-column
-          prop="productName"
-          align="center"
+          header-align="center"
+          align="left"
           label="购买产品">
+          <template slot-scope="scope">
+            {{nameObject[scope.row.productId]}}
+          </template>
         </el-table-column>
         <el-table-column
-          prop="channel"
+          prop="channelId"
           width="160"
           :resizable="false"
           align="center"
           label="购买渠道">
         </el-table-column>
         <el-table-column
-          prop="quantity"
           width="120"
           :resizable="false"
-          align="center"
+          align="right"
+          header-align="center"
           label="购买数量">
+          <template slot-scope="scope">
+            {{scope.row.quantity}}{{unitObject[scope.row.productId]}}
+          </template>
         </el-table-column>
         <el-table-column
           prop="amount"
           width="120"
           :resizable="false"
-          align="center"
+          align="right"
+          header-align="center"
           label="购买金额">
         </el-table-column>
         <el-table-column
@@ -120,18 +127,17 @@
           :resizable="false"
           width="100">
           <template slot-scope="scope">
-            <el-button type="text" size="small">编辑</el-button>
-            <el-button type="text" size="small">删除</el-button>
+            <el-button type="text" size="small" @click="editSales(scope.row)">编辑</el-button>
+            <el-button type="text" size="small" @click="deleteSales(scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
       <el-pagination
-        @size-change="pageSizeChange"
         @current-change="pageChange"
-        :current-page.sync="page"
-        :page-size="10"
+        :current-page.sync="currentPage"
+        :page-size="20"
         layout="prev, pager, next, jumper"
-        :total="1000">
+        :total="pageCount">
       </el-pagination>
     </div>
     <el-dialog
@@ -141,30 +147,37 @@
       <span>
         <el-form ref="salesForm" label-position="right" :rules="rules" label-width="120px" :model="salesForm">
           <el-form-item label="销售日期：" prop="time">
-            <el-date-picker
-              v-model="salesForm.time"
-              size="mini"
-              style="width: 100%;"
-              type="date"
-              :clearable="false"
-              placeholder="选择日期">
-            </el-date-picker>
+<!--            <el-date-picker-->
+<!--              v-model="salesForm.time"-->
+<!--              size="mini"-->
+<!--              type="date"-->
+<!--              :clearable="false"-->
+<!--              placeholder="选择日期">-->
+<!--            </el-date-picker>-->
+                <el-date-picker
+                  v-model="salesForm.time"
+                  style="width: 100%;"
+                  size="mini"
+                  value-format="yyyy-MM-dd HH:mm:ss"
+                  type="datetime"
+                  placeholder="选择日期时间">
+                </el-date-picker>
           </el-form-item>
-          <el-form-item label="买家姓名：" prop="buyer">
-            <el-input v-model="salesForm.buyer" size="mini" placeholder="请输入买家姓名"></el-input>
+          <el-form-item label="买家姓名：" prop="buyers">
+            <el-input v-model="salesForm.buyers" size="mini" placeholder="请输入买家姓名"></el-input>
           </el-form-item>
-          <el-form-item label="产品名称：" prop="productName">
+          <el-form-item label="产品名称：" prop="productId">
             <dropTree
               :class="prodFlag ? 'dropError' : ''"
               placeholder="请选择产品名称"
               clearable
               :options="productName.options"
-              v-model="salesForm.productName"
+              v-model="salesForm.productId"
               @change="productValidate"
             ></dropTree>
           </el-form-item>
-          <el-form-item label="销售途径：" prop="channel">
-            <el-select size="mini" style="width: 100%;" v-model="salesForm.channel" placeholder="请选择销售途径">
+          <el-form-item label="销售途径：" prop="channelId">
+            <el-select size="mini" style="width: 100%;" v-model="salesForm.channelId" placeholder="请选择销售途径">
               <el-option
                 v-for="item in channel.options"
                 :key="item.value"
@@ -194,7 +207,7 @@
       </span>
       <span slot="footer" class="dialog-footer">
         <el-button @click="showModel = false">取 消</el-button>
-        <el-button type="primary" @click="confirmEdit('salesForm')">确 定</el-button>
+        <el-button type="primary" @click="confirmModel">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -210,16 +223,19 @@ export default {
   },
   data(){
     return {
+      searchParams:{
+        productId:[],
+        channelId:[],
+        buyers:'',
+        seller:[],
+        time:'',
+      },
       productName:{
         options:require('../../util/baseData').default.teaType,
-        value:[]
       },
       channel:{
         options:require('../../util/baseData').default.channel,
-        value:[]
       },
-      buyer:'',
-      salesTime:'',
       memberName:{
         options:[
           {
@@ -235,114 +251,49 @@ export default {
             value:'许佩清'
           },
         ],
-        value:[],
       },
       salesData:[
         {
-          time:'2021-10-20',
-          buyer:'xx',
-          productName:'绿茶',
-          channel:'店面',
-          quantity:'1000g',
+          id:'1',
+          time:'2021-10-20 14:20:00',
+          buyers:'xx',
+          productId:'H1-6-4',
+          productName:'王大榜特级滇红大金针',
+          channelId:'店面',
+          quantity:'1000',
           amount:360,
           seller:'彭旭灿',
         },
         {
-          time:'2021-10-20',
-          buyer:'xx',
-          productName:'绿茶',
-          channel:'店面',
-          quantity:'1000g',
+          id:'2',
+          time:'2021-10-20 14:20:00',
+          buyers:'xx',
+          productId:'H1-6-4',
+          productName:'王大榜特级滇红大金针',
+          channelId:'店面',
+          quantity:'1000',
           amount:360,
           seller:'彭旭灿',
         },
         {
-          time:'2021-10-20',
-          buyer:'xx',
-          productName:'绿茶',
-          channel:'店面',
-          quantity:'1000g',
+          id:'3',
+          time:'2021-10-20 14:20:00',
+          buyers:'xx',
+          productId:'H1-6-4',
+          productName:'王大榜特级滇红大金针',
+          channelId:'店面',
+          quantity:'1000',
           amount:360,
           seller:'彭旭灿',
         },
         {
-          time:'2021-10-20',
-          buyer:'xx',
-          productName:'绿茶',
-          channel:'店面',
-          quantity:'1000g',
-          amount:360,
-          seller:'彭旭灿',
-        },
-        {
-          time:'2021-10-20',
-          buyer:'xx',
-          productName:'绿茶',
-          channel:'店面',
-          quantity:'1000g',
-          amount:360,
-          seller:'彭旭灿',
-        },
-        {
-          time:'2021-10-20',
-          buyer:'xx',
-          productName:'绿茶',
-          channel:'店面',
-          quantity:'1000g',
-          amount:360,
-          seller:'彭旭灿',
-        },
-        {
-          time:'2021-10-20',
-          buyer:'xx',
-          productName:'绿茶',
-          channel:'店面',
-          quantity:'1000g',
-          amount:360,
-          seller:'彭旭灿',
-        },
-        {
-          time:'2021-10-20',
-          buyer:'xx',
-          productName:'绿茶',
-          channel:'店面',
-          quantity:'1000g',
-          amount:360,
-          seller:'彭旭灿',
-        },
-        {
-          time:'2021-10-20',
-          buyer:'xx',
-          productName:'绿茶',
-          channel:'店面',
-          quantity:'1000g',
-          amount:360,
-          seller:'彭旭灿',
-        },
-        {
-          time:'2021-10-20',
-          buyer:'xx',
-          productName:'绿茶',
-          channel:'店面',
-          quantity:'1000g',
-          amount:360,
-          seller:'彭旭灿',
-        },
-        {
-          time:'2021-10-20',
-          buyer:'xx',
-          productName:'绿茶',
-          channel:'店面',
-          quantity:'1000g',
-          amount:360,
-          seller:'彭旭灿',
-        },
-        {
-          time:'2021-10-20',
-          buyer:'xx',
-          productName:'绿茶',
-          channel:'店面',
-          quantity:'1000g',
+          id:'4',
+          time:'2021-10-20 14:20:00',
+          buyers:'xx',
+          productId:'H1-6-4',
+          productName:'王大榜特级滇红大金针',
+          channelId:'店面',
+          quantity:'1000',
           amount:360,
           seller:'彭旭灿',
         },
@@ -351,47 +302,71 @@ export default {
 
       salesForm:{
         time:'',
-        buyer:'',
-        productName:'',
-        channel:'',
+        buyers:'',
+        productId:'',
+        channelId:'',
         quantity:'',
         amount:'',
         seller:'',
       },
       rules: {
-        // time: [{required: true, message: '销售日期必选', trigger: 'blur'}],
-        // buyer: [{required: true, message: '必填', trigger: 'blur'}],
-        productName: [{required: true, message: '产品名称必选', trigger: 'change'}],
-        channel: [{required: true, message: '销售途径必选', trigger: 'change'}],
+        time: [{required: true, message: '销售时间必选', trigger: 'blur'}],
+        productId: [{required: true, message: '产品名称必选', trigger: 'change'}],
+        channelId: [{required: true, message: '销售途径必选', trigger: 'change'}],
         quantity: [{required: true, message: '购买数量必填', trigger: 'blur'}],
         amount: [{required: true, message: '购买金额必填', trigger: 'blur'}],
         seller: [{required: true, message: '销售人员必选', trigger: 'change'}],
       },
-      editIndex:0,
       modelType:'add',
       showModel:false,
       prodFlag:false,
       currentUnit:'g',
-      page:1,
-      unitObject:{
-
-      },
+      currentPage:1,
+      pageCount:1,
+      unitObject:{},
+      nameObject:{},
 
 
     }
   },
   methods:{
-    validateFlag(){
-      this.prodFlag = this.salesForm.productName === ''
+    clickSearch(){
+      this.currentPage = 1
+      this.searchTable()
+    },
+    resetSearch(){
+      this.searchParams = {
+        productId:[],
+        channelId:[],
+        buyers:'',
+        seller:[],
+        time:'',
+      }
+    },
+    pageChange(){
+      this.searchTable()
+    },
+    searchTable(){
+      let params = {
+        ...this.searchParams,
+        pageSize:100,
+        page:this.currentPage,
+      }
+      console.log(params, '查询销售记录表格');
     },
     productValidate(val){
       this.prodFlag = val === ''
       this.currentUnit = val === '' ? '' : this.unitObject[val]
-      this.$refs.salesForm.validateField('productName')
+      this.$refs.salesForm.validateField('productId')
     },
-    confirmEdit(formName) {
-      this.validateFlag()
-      this.$refs[formName].validate((valid) => {
+    confirmModel() {
+      this.prodFlag = this.salesForm.productId === ''
+      let params = {
+        ...this.salesForm,
+      }
+      if(this.modelType !== 'add') params.id = this.modelType
+      console.log(params);
+      this.$refs.salesForm.validate((valid) => {
         if (valid) {
           this.showModel = false
         } else {
@@ -401,24 +376,65 @@ export default {
       })
     },
     addSales(){
-      this.salesForm.time = this.utils.setTimeFilter(new Date())
-      console.log(this.salesForm.time);
+      this.salesForm.time = this.utils.setTimeFilter(new Date()) + ' 12:00:00'
+      this.modelType = 'add'
+      this.salesForm = {
+        time:'',
+        buyers:'',
+        productId:'',
+        channelId:'',
+        quantity:'',
+        amount:'',
+        seller:'',
+      }
+      this.currentUnit = ''
       this.showModel = true
       this.$nextTick(()=>{
         this.$refs.salesForm.clearValidate()
       })
 
     },
-
-    pageSizeChange(){
-
+    editSales(data){
+      console.log(data);
+      this.modelType = data.id
+      this.salesForm = {
+        time:data.time,
+        buyers:data.buyers,
+        productId:data.productId,
+        channelId:data.channelId,
+        quantity:data.quantity,
+        amount:data.amount,
+        seller:data.seller,
+      }
+      this.currentUnit = this.unitObject[data.productId]
+      this.showModel = true
+      this.$nextTick(()=>{
+        this.$refs.salesForm.clearValidate()
+      })
     },
-    pageChange(){
+    deleteSales(data){
+      this.$confirm('此操作将永久删除该条记录, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        console.log(data.id);
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
 
     },
     getEveryUnit(array){
       array.forEach(item=>{
         this.unitObject[item.value] = item.unit
+        this.nameObject[item.value] = item.label
         if(item.children && item.children.length !== 0){
           this.getEveryUnit(item.children)
         }
@@ -427,7 +443,6 @@ export default {
   },
   mounted() {
     this.getEveryUnit(this.productName.options)
-    console.log(this.unitObject);
 
 
   }
